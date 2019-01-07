b689b86f18592a7423c676e08d3619a15a965816
bug <bug@DESKTOP-R5E0MHI>
2019-01-08 00:10:59 +0800

    使webpack支持scss

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>ddd</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style rel="stylesheet/scss" lang="scss">
+
+</style>
