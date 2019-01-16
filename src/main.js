35f63ab0c38d1d6bec91e4c68a57873680695848
bug <bug@DESKTOP-R5E0MHI>
2019-01-16 22:06:13 +0800

    登录页面样式、icons

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import '@/icons' // icon
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
